f59cfc961f4d9f0c238f63a49953429537a1f9d0
Rituvendra-mishra <rituvendra13@gmail.com>
2022-07-18 15:53:34 +0530

    update folder structure

--- controller/controller.js
+++ controller/controller.js
@@ -70,19 +70,3 @@ res.status(500).json(err)
 
 module.exports=router
 
-
-const verifyToken = (req, res, next) => {
-  let token = req.headers["authorization"];
-  console.log("token",token);
-  if (!token) {
-    return res.status(403).send({ message: "No token provided!" });
-  }
-  jwt.verify(token,  "shhhhh", (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ message: "Unauthorized!" });
-    }
-    req.userId = decoded.id;
-    next();
-  });
- 
-};
---
+++ middleware/verfifyToken.js
@@ -0,0 +1,16 @@
+
+const verifyToken = (req, res, next) => {
+    let token = req.headers["authorization"];
+    console.log("token",token);
+    if (!token) {
+      return res.status(403).send({ message: "No token provided!" });
+    }
+    jwt.verify(token, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ message: "Unauthorized!" });
+      }
+      req.userId = decoded.id;
+      next();
+    });
+   
+  };

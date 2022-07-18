31f63f88a943e0c73568c5f2de327a6b47483d85
Rituvendra-mishra <rituvendra13@gmail.com>
2022-07-18 15:28:03 +0530

    remove node_modules

---
+++ controller/controller.js
@@ -0,0 +1,88 @@
+const router=require("express").Router()
+const User=require("../models/User")
+const bcrypt=require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+//register
+router.post("/register", async(req,res)=>{
+    try{
+        const {username,email,password,cpassword}=req.body
+        const pass  = await bcrypt.hash(password,10)
+        const user = await User.create({
+            username,
+            email,
+            password:pass ,
+            cpassword
+          });
+          console.log(user,"user");
+          return res.status(200).json({
+            message: "succes",
+          });
+        } catch (error) {
+            console.log(error)
+          return res.status(500).json({
+            error: error.message,
+          });
+  
+}}
+)
+
+
+
+//login
+router.post("/login",async(req,res)=>{
+    
+ try{
+ const user=await User.findOne({ email:req.body.email})
+ !user && res.status(400).json("wrong credential")
+
+ const validate=await bcrypt.compare(req.body.password,user.password)
+ !validate && res.status(400).json("Wrong password")
+//  const{password ,...others}=user
+
+const token = await jwt.sign({userid:user.id},"mynameisrituvendramishrajabalpur",{
+  expiresIn:"2h"
+})
+user.token=token
+const setToken =  await User.findOneAndUpdate({_id:user.id},{$set:{token:token}})
+// console.log(token)
+const userver=await jwt.verify(token,"mynameisrituvendramishrajabalpur")
+// console.log(userver)
+res.status(200).json({"accessToken":token,"user":user})
+
+ 
+ 
+ 
+//  const createWebToken= async(id)=>{
+//    const token = await jwt.sign({userid:id},"mynameisrituvendramishrajabalpur")
+//    const setToken =  await User.findOneAndUpdate({_id:id},{$set:{token:token}})
+// }
+//   createWebToken(user.id)
+} catch (err){
+  console.log(err)
+res.status(500).json(err)
+}
+
+
+
+})
+
+
+module.exports=router
+
+
+const verifyToken = (req, res, next) => {
+  let token = req.headers["authorization"];
+  console.log("token",token);
+  if (!token) {
+    return res.status(403).send({ message: "No token provided!" });
+  }
+  jwt.verify(token,  "shhhhh", (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized!" });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+ 
+};
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const authrouter=require("./routes/auth")
 
 const cors = require('cors')
  
@@ -12,7 +11,7 @@ app.use(cors())
 dotenv.config();
 app.use(bodyParser.json());
 app.use(express.json());
-app.use("/api/auth",authrouter)
+
 mongoose.connect(`mongodb+srv://Rituvendra_login:rituvendra12345@cluster0.n518x.mongodb.net/users_db`)
   .then(()=>console.log("DB connected"))
   .catch((err) => console.log('============>',err));
